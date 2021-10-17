31ef6ef447f8f112ce33d1839b50d0ea1fa6a1cc
Lin Lyu <lyulin.lynn@gmail.com>
2021-10-18 01:35:04 +0200

    Loops

--- src/com/linlyu/ControlFlow.java
+++ src/com/linlyu/ControlFlow.java
@@ -1,10 +1,17 @@
 package com.linlyu;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ControlFlow {
     public static void main(String[] args) {
-        comparisonOperations();
-        logicalOperators();
-        ifStatements();
+//        comparisonOperations();
+//        logicalOperators();
+//        ifStatements();
+//        forLoops();
+//        whileLoops();
+//        whileLoops2();
+        forEachLoops();
     }
 
     public static void comparisonOperations() {
@@ -79,4 +86,58 @@ public class ControlFlow {
 
         }
     }
+
+    public static void forLoops() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World " + i);
+        } // when you know how many times you want to execute statements, using for loops.
+    }
+
+    public static void whileLoops() {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")) {
+            // can not use != since it will compare address. For strings, using .equal
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            if (!input.equals("quit"))
+                System.out.println(input);
+        }
+        // using while loops when we don't know exactly how many times you want to repeat something.
+        // First check the condition, then execute the statement.
+
+        do {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
+        // First execute the statement, then check the condition.
+
+        // the break; statement terminates a loop,
+        // and the continue; statement moves control to the beginning of a loop.
+    }
+
+    public static void whileLoops2 () {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            if (input.equals("pass"))
+                continue;
+            if (input.equals("quit"))
+                break;
+            System.out.println(input);
+        }
+    }
+
+    public static void forEachLoops () {
+        String[] fruits = {"Apple", "Mango", "Orange"};
+        for (int i = 0; i < fruits.length; i++)
+            System.out.println(fruits[i]);
+
+        for (String fruit : fruits)
+            System.out.println(fruit);
+    }
+
 }
